b7d36f037832d89e77cf17129afebb81e1d35a89
Abdallah Sherif <118034215+AbdallahSherif11@users.noreply.github.com>
2025-04-18 02:41:06 +0200

    [1342] [Time Beats: 100.00%] [Memory Beats: 22.88%] - LeetPush

---
+++ Number-of-Steps-to-Reduce-a-Number-to-Zero.cs
@@ -0,0 +1,21 @@
+public class Solution 
+{
+    public int NumberOfSteps(int num) 
+    {
+        int steps = 0; 
+        while (num > 0)
+        {
+            if (num % 2 == 0)
+            {
+                num = num / 2;
+                steps++;
+            }
+            else
+            {
+                num--;
+                steps++;
+            }
+        }
+        return steps;
+    }   
+}
