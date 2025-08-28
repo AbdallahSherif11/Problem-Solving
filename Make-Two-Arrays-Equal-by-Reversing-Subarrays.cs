c82f752495099f6b751e0d488a6343f179d24b87
Abdallah Sherif <118034215+AbdallahSherif11@users.noreply.github.com>
2025-08-28 03:13:02 +0300

    [1460] [Time Beats: 52.17%] [Memory Beats: 95.65%] - LeetPush

---
+++ Make-Two-Arrays-Equal-by-Reversing-Subarrays.cs
@@ -0,0 +1,15 @@
+public class Solution 
+{
+    public bool CanBeEqual(int[] target, int[] arr) 
+    {
+        Array.Sort(target);
+        Array.Sort(arr);
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (target[i] != arr[i])
+                return false;
+        }
+        return true;
+    }
+}
