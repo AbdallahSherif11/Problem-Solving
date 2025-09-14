86d4a94b0257ee4c1fc030e96e6b6ce06facb41b
Abdallah Sherif <118034215+AbdallahSherif11@users.noreply.github.com>
2025-09-14 22:54:39 +0300

    [944] [Time Beats: 95.65%] [Memory Beats: 60.87%] - LeetPush

---
+++ Delete-Columns-to-Make-Sorted.cs
@@ -0,0 +1,20 @@
+public class Solution 
+{
+    public int MinDeletionSize(string[] strs) 
+    {
+        int Result = 0;
+        for (int j = 0; j < strs[0].Length; j++)
+        {
+            for (int i = 0; i < strs.Length - 1; i++)
+            {
+                if (strs[i][j] > strs[i+1][j])
+                {
+                    Result++;
+                    break;
+                }
+            }
+        }
+
+        return Result;
+    }
+}
