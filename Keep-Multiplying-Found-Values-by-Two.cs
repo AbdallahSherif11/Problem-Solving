e35174d380cb5766174060fe627c3e79ea7091dd
Abdallah Sherif <118034215+AbdallahSherif11@users.noreply.github.com>
2025-11-15 19:02:31 +0200

    [2154] [Time Beats: 100.00%] [Memory Beats: 83.08%] - LeetPush

---
+++ Keep-Multiplying-Found-Values-by-Two.cs
@@ -0,0 +1,15 @@
+public class Solution 
+{
+    public int FindFinalValue(int[] nums, int original) 
+    {
+        for(int i  = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == original)
+            {
+                original *= 2;
+                i = -1;
+            }
+        }
+        return original;
+    }
+}
