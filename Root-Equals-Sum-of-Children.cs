e93fcb311501d50168a0fc9cad6e520a6a779d0b
Abdallah Sherif <118034215+AbdallahSherif11@users.noreply.github.com>
2026-01-22 17:55:53 +0200

    [2236] [Time Beats: 100.00%] [Memory Beats: 87.79%] - LeetPush

---
+++ Root-Equals-Sum-of-Children.cs
@@ -0,0 +1,20 @@
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     public int val;
+5 *     public TreeNode left;
+6 *     public TreeNode right;
+7 *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+8 *         this.val = val;
+9 *         this.left = left;
+10 *         this.right = right;
+11 *     }
+12 * }
+13 */
+14public class Solution 
+15{
+16    public bool CheckTree(TreeNode root) 
+17    {
+18        return root.val == (root.left.val +  root.right.val);
+19    }
+20}
